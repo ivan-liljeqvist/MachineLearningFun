da07d3f1006655cea5d0917f13f039ab5ec22b1b
Ivan Liljeqvist <ivanli@kth.se>
2016-07-05 11:55:35 +0200

    multinomial logistic regression done

--- 1/main.py
+++ 1/main.py
@@ -45,19 +45,29 @@ def model(data,w_logit,b_logit):
     #Assemble the NN
     return tf.matmul(data,w_logit)+b_logit
 
+# Helps to understand how accurate something is
+def accuracy(predictions,labels):
+    return (100.0 * np.sum(np.argmax(predictions,1)==np.argmax(labels,1))/predictions.shape[0]);
+
 
 def multinomialLogisticRegression():
+
     #PART 1: Describe the graph
+
     graph = tf.Graph()
     with graph.as_default():
 
         #Graph variables
-        batch_size = 128
+
+        batch_size = 128 #small batch that will be chosen randomly
         beta = 0.001 #regularization
         image_size = 28
         num_labels = 10
 
+        learning_rate = 0.5 # for Gradient Descent
+
         #Create a placeholder that will be fed with training minibatches at run time
+
         tf_train_dataset = tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
         tf_train_labels = tf.placeholder(tf.float32,shape=(batch_size,num_labels))
         tf_valid_dataset = tf.constant(mnist.validation.images)
@@ -68,6 +78,52 @@ def multinomialLogisticRegression():
         b_logit = tf.Variable(tf.zeros([num_labels]))
 
 
+        #logit = predictions, we make prediction and calculate the error
 
-plotImage(5)
+        logits = model(tf_train_dataset, w_logit, b_logit) # make a prediction
+
+        # having made the prediction we need to calculate how correct it is
+        # the labels are the correct answers and we compare them to the prediction that were made
+        # softmax_cross_entropy is a common error function
+
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits,tf_train_labels))
+        regularized_loss = tf.nn.l2_loss(w_logit)
+        total_loss = loss + beta * regularized_loss
+
+        # Now when we know the error we can use Gradient Descent to adjust
+
+        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(total_loss);
+
+        # Predictions for the training, validation and test data
+
+        train_prediction = tf.nn.softmax(logits)
+        valid_prediction = tf.nn.softmax(model(tf_valid_dataset, w_logit, b_logit))
+        test_prediction = tf.nn.softmax(model(tf_test_dataset, w_logit, b_logit))
+
+        # PART 2: Train
+
+        num_steps = 5000
+
+        with tf.Session(graph=graph) as session:
+            tf.initialize_all_variables().run()
+            for step in range(num_steps):
+
+                # Generate a minibatch
+                batch_data, batch_labels = mnist.train.next_batch(batch_size);
+
+                feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+
+                #RUN IN THE GRAPH
+                _, l, predictions = session.run([optimizer,loss,train_prediction],feed_dict=feed_dict)
+
+                if(step % 500 == 0):
+                    print("Minibatch loss at step %d: %f" % (step,l))
+                    print("Minibatch accuracy: %f" % accuracy(predictions,batch_labels))
+                    print("Validation accuracy: %f" % accuracy(valid_prediction.eval(),mnist.validation.labels))
+                    print("Test accuracy: %f" % accuracy(test_prediction.eval(),mnist.test.labels))
+                    print("------\n\n")
+
+
+
+multinomialLogisticRegression()
 
